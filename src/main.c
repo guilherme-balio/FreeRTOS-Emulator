633373bf8751959225701da17a932e5ac98ea1ee
Guilherme Balio <45636618+guilherme-balio@users.noreply.github.com>
2022-12-04 21:49:49 -0300

    limpando um pouco a main

--- src/main.c
+++ src/main.c
@@ -4,51 +4,6 @@
 #include <time.h>
 
 #include "main.h"
-#include "queue.h"
-#include "task.h"
-
-#include "TUM_Ball.h"
-#include "TUM_Draw.h"
-#include "TUM_Font.h"
-#include "TUM_Event.h"
-#include "TUM_Sound.h"
-#include "TUM_Utils.h"
-#include "TUM_FreeRTOS_Utils.h"
-#include "TUM_Print.h"
-
-#include "AsyncIO.h"
-
-#include "defines.h"
-#include "demo_tasks.h"
-#include "async_sockets.h"
-#include "async_message_queues.h"
-#include "draw.h"
-#include "buttons.h"
-
-
-#ifdef TRACE_FUNCTIONS
-#include "tracer.h"
-#endif
-
-const unsigned char next_state_signal = NEXT_TASK;
-const unsigned char prev_state_signal = PREV_TASK;
-
-SemaphoreHandle_t DrawSignal = NULL;
-
-void vSwapBuffers(void *pvParameters)
-{
-    TickType_t xLastWakeTime;
-    xLastWakeTime = xTaskGetTickCount();
-    const TickType_t frameratePeriod = 20;
-
-    while (1) {
-        tumDrawUpdateScreen();
-        tumEventFetchEvents(FETCH_EVENT_BLOCK);
-        xSemaphoreGive(DrawSignal);
-        vTaskDelayUntil(&xLastWakeTime,
-                        pdMS_TO_TICKS(frameratePeriod));
-    }
-}
 
 /* Kernel includes. */
 #include "FreeRTOS.h"
@@ -59,14 +14,14 @@ void vSwapBuffers(void *pvParameters)
 #define NUM_OF_PHILOSOPHERS (5)
 
 SemaphoreHandle_t forks[NUM_OF_PHILOSOPHERS];
-SemaphoreHandle_t entry_sem;
-TaskHandle_t philosophers[NUM_OF_PHILOSOPHERS];
+SemaphoreHandle_t arbitro;
 int comeu = 0;
 
 #define left(i) (i)
 #define right(i) ((i + 1) % NUM_OF_PHILOSOPHERS)
 
 void take_fork(int i) {
+    //xSemaphoreTake(arbitro,portMAX_DELAY);
 	xSemaphoreTake(forks[left(i)], portMAX_DELAY);
 	xSemaphoreTake(forks[right(i)], portMAX_DELAY);
 	//printf("Philosopher %d got the fork %d and %d\n", i, left(i), right(i));
@@ -76,6 +31,7 @@ void take_fork(int i) {
 void put_fork(int i) {
     //printf("Philosopher %d is eating\n", i);
     vTaskDelay(10000);
+    //xSemaphoreGive(arbitro);
 	xSemaphoreGive(forks[left(i)]);
 	xSemaphoreGive(forks[right(i)]);
     comeu++;
@@ -95,8 +51,6 @@ void philosophers_task(void *param) {
 	while (1) {
         
         pensar = gen_random(0, 100);
-        //printf("Contador de pensar: %d\n", pensar);
-
         vTaskDelay(pensar);
 
         // pega garfo
@@ -118,9 +72,8 @@ void task_measure(void *param){
 
         printf("%d,", xLastWakeTime/10000);
         printf("%d \n", comeu);
-        // alterando o número de filósofos
 
-        // Wait for the next cycle.
+        // Esperar próximo ciclo
         vTaskDelayUntil( &xLastWakeTime, xFrequency );
     }
 }
@@ -131,23 +84,15 @@ int main(void) {
 	int param[NUM_OF_PHILOSOPHERS];
     srand(time(NULL));
 
-
-	// Create Five Semaphores for the five shared resources. 
-	// Which is the fork in this case.
+	//arbitro = xSemaphoreCreateMutex();
 
 	for (i = 0; i < NUM_OF_PHILOSOPHERS; i++) {
 		forks[i] = xSemaphoreCreateMutex();
 	}
 
-	// This is the critical piece to avoid deadlock.
-	// If one less philosopher is allowed to act then there will no deadlock.
-	// As one philosopher will always get two forks and so it will go on.
-
     xTaskCreate(task_measure, "medição", 30, NULL, 1, NULL);
 
 	for (i = 0; i < NUM_OF_PHILOSOPHERS; i++) {
-		// Ofcourse, you can just pass i as every thread needs it's own
-		// address to store the parameter.
 		param[i] = i;
 		xTaskCreate(philosophers_task, "filósofos", 30, &(param[i]), 2, NULL);
 	}
@@ -155,16 +100,10 @@ int main(void) {
     return 0;
 }
 
-//int main(int argc, char *argv[])
-//{
-    
-
-//}
-
 // cppcheck-suppress unusedFunction
 __attribute__((unused)) void vMainQueueSendPassed(void)
 {
-    /* This is just an example implementation of the "queue send" trace hook. */
+    // This is just an example implementation of the "queue send" trace hook. 
 }
 
 // cppcheck-suppress unusedFunction
